7a9edc76ddeaa580d940fdee04f62633033ab084
LukaRic <lukajacobsen1712@gmail.com>
2026-03-09 09:58:57 +0100

    Added label

--- states/leaderboard_menu.py
+++ states/leaderboard_menu.py
@@ -5,6 +5,7 @@ from ui.background import ResponsiveParallexGrid
 from states.base_state import BaseState
 from utils.settings import Settings
 from ui.button import Button
+from ui.label import Label
 from game import Game
 
 class LeaderboardMenu(BaseState):
@@ -24,13 +25,31 @@ class LeaderboardMenu(BaseState):
 
         super().__init__(game)
 
+        self.game = game
+
+        # Define fonts
+        self.font = pg.font.SysFont('Verdana', 24)
+
         # Grab settings values
         self.colors = Settings.get('color_palette')
+        self.screen_width, self.screen_height = Settings.get('window_size').values()
 
         # Init responsive background class
         self.background = ResponsiveParallexGrid(cell_size=40, max_speed=50)
 
-        self.buttons = []
+        self.leaderboard_label = Label(
+            'Test text', (self.screen_width / 2, self.screen_height / 2),
+            font=self.font, size=(300, 200)
+        )
+
+        self.buttons = [
+            Button(
+                text='Back', center_pos=(20 + (self.screen_width / 10), self.screen_height - 55),
+                callback=self.back, font=self.font,
+                size=(self.screen_width / 5, 50), bg_color=self.colors['blue'],
+                hover_color=self.colors['blue_light']
+            )
+        ]
 
         lr.Log.debug('Leaderboard menu initialized!')
 
@@ -40,10 +59,14 @@ class LeaderboardMenu(BaseState):
 
     # <-----> State Methods <-----> #
     def handle_events(self, events:list[pg.event.Event]):
-        pass
+        for button in self.buttons:
+            button.handle_events(events)
 
     def update(self, delta_time:float):
         mouse_pos = pg.mouse.get_pos()
+
+        for button in self.buttons:
+            button.update(mouse_pos)
 
         self.background.update(delta_time, mouse_pos)
     
@@ -51,3 +74,9 @@ class LeaderboardMenu(BaseState):
         # Reset background
         screen.fill(self.colors['background'])
         self.background.draw(screen)
+
+        # Draw buttons
+        for button in self.buttons:
+            button.draw(screen)
+
+        self.leaderboard_label.draw(screen)
---
+++ ui/label.py
@@ -0,0 +1,42 @@
+import loggerric as lr
+import pygame as pg
+
+from utils.settings import Settings
+
+class Label:
+    def __init__(
+        self, text:str, center_pos:tuple, font:pg.font.Font,
+        padding:int=12, size:tuple=None, bg_color:tuple=None,
+        text_color:tuple=None, outline:int=3, outline_color:tuple=None
+    ):
+        # Increase scope
+        self.text    = text
+        self.font    = font
+        self.size    = size or (padding * 2, padding * 2)
+        self.padding = padding
+        self.outline = outline
+
+        # Set colors
+        self.colors      = Settings.get('color_palette')
+        self.bg_color    = bg_color or self.colors['primary_accent']
+        self.text_color  = text_color or self.colors['dark_text']
+        self.outline_color = outline_color or self.colors['secondary_accent']
+
+        # Graphics
+        self.text_surface = self.font.render(text, True, self.text_color)
+        self.rect         = pg.Rect(0, 0, *self.size)
+        self.rect.center  = center_pos
+        self.text_rect    = self.text_surface.get_rect(center=self.rect.center)
+
+        lr.Log.debug('Initialized label:', f'{text[0:7]}...' if len(text) > 10 else text)
+
+    # <-----> State Methods <-----> #
+    def update(self, mouse_pos:tuple[int, int]):
+        pass
+
+    def draw(self, screen:pg.Surface):
+        pg.draw.rect(screen, self.bg_color, self.rect, border_radius=10)
+
+        pg.draw.rect(screen, (0, 0, 0), self.rect, width=self.outline, border_radius=10)
+
+        screen.blit(self.text_surface, self.text_rect)
