811d6d6f2ee81bac7cc4127db919ed0d585d352e
LukaRic <lukajacobsen1712@gmail.com>
2026-03-23 08:25:13 +0100

    Added WASD support

--- systems/input_handler.py
+++ systems/input_handler.py
@@ -18,6 +18,11 @@ class InputHandler:
             pg.K_DOWN: (0, 1),
             pg.K_LEFT: (-1, 0),
             pg.K_RIGHT: (1, 0),
+
+            pg.K_w: (0, -1),
+            pg.K_s: (0, 1),
+            pg.K_a: (-1, 0),
+            pg.K_d: (1, 0)
         }
 
     # Called at snake move (10fps ish)
